40244008b7eb5fdfb2f325f36f531fd44caa79a0
wangzl <wzlas111@126.com>
2014-09-10 23:42:22 +0800

    0910

--- src/com/eastelsoft/tv/widget/ESLoading.java
+++ src/com/eastelsoft/tv/widget/ESLoading.java
@@ -1,20 +1,40 @@
 package com.eastelsoft.tv.widget;
 
+import com.eastelsoft.tv.R;
+
 import android.content.Context;
 import android.util.AttributeSet;
 
 public class ESLoading extends ESLoadingView {
 
+	private int loadingSize;
+	
 	public ESLoading(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		init();
 	}
 
 	public ESLoading(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public ESLoading(Context context) {
 		super(context);
+		init();
+	}
+	
+	private void init() {
+		loadingSize = dip2px(48.0F);
+		setBackgroundResource(R.drawable.play_loading_big);
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		setMeasuredDimension(loadingSize, loadingSize);
 	}
 
+	public int dip2px(float paramFloat){
+		return (int) (0.5F + paramFloat * getContext().getResources().getDisplayMetrics().density);
+	}
 }
